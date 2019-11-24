b06844eabd05ca5b25ea10729744f5d893d65988
grigory <petrenkogrigory18@gmail.com>
2019-11-24 14:54:34 +0100

    8 correctly

--- Bash/2019A/scrypt8.sh
+++ Bash/2019A/scrypt8.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
 
-#int=$(^-?[0-9]+$)
+int="^-?[0-9]+$"
 
-if ! [[ "$1" =~ ^-?[0-9]+$ ]]   #   [[ "$1" =~ "^[0-9]+$" && "$2" =~ "^[0-9]+$" ]]
+#echo $int
+
+if [[ "$1" =~ ${int} || "$2" =~ ${int} ]]   
+#if [[ "$1" =~ ^-?[0-9]+$ && "$2" =~ ^-?[0-9]+$ ]]  
+
 then
-	echo "the first parameter has to be an integer"
-#elif ! [[ "$2" =~ ^[0-9}+$ ]]
-#then
-#	echo "the second parameter has to be integer too"
-# else	"the parameters have to be an integer" 
-else 
-
 	argument_one=$1
-	argument_two=$2
+        argument_two=$2
 	sum=$(( $argument_one + $argument_two ))
 	multiply=$(( $argument_one * $argument_two ))
 
 	echo "The sum of arguments $sum"
 	echo "The argument multiplication $multiply"
 
-	function SumFunction(){
+		function SumFunction(){
 		sumfunct=$(( $1 + $2 ))
-		echo " The sum of arguments $sumfunct "
+	echo " The sum of arguments $sumfunct "
 
-		}
-	SumFunction argument_one argument_two
+		 }
+		SumFunction argument_one argument_two
 
-	function MultiplyFunction(){
+		function MultiplyFunction(){
 		mufunc=$(( $1 * $2 ))
 		echo " The argument multiplication $mufunc "
-		}	
+		 }
 	MultiplyFunction argument_one argument_two
+else
+	echo " one of parameters not integer "
 fi
