bc4b1f237eb81da89b2045d0a0ecff81dcfd61a8
grigory <petrenkogrigory18@gmail.com>
2021-06-11 23:15:04 +0200

    _

--- HackerRank_Linux/008_Compute_the_Average.sh
+++ HackerRank_Linux/008_Compute_the_Average.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-#sum=0
-#read n
-#while read -r line || [[ -n "$line" ]]; do
-#    sum=$(($sum + $line))
-#done
-#printf "%.3f" $(echo "scale=10; $sum/$n" | bc -l)
+
 
 read n
 arr=($(cat)) 
@@ -14,16 +9,4 @@ printf "%.3f" $(echo $((${arr// /+}))/$n | bc -l)
 
 
 
-
-#input: 100
-#9829
-#8093
-#8839
-#4794
-#7444
-#7590
-#1684
-#9283
-#7603
-#6751
-#7100
+#ctrl+d to end of input
